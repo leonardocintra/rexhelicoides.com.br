51f4217f9f5a2fe21d192eb7fba4334521570234
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-05-09 13:17:05 -0300

    :tada: add zap dos mano

--- src/components/custom/home/opinioes.tsx
+++ src/components/custom/home/opinioes.tsx
@@ -7,7 +7,7 @@ const opinioes = [
   {
     id: 1,
     nome: "Lucas Carrijo",
-    whastapp: "(35) 9 9991-0614",
+    whastapp: "(35) 99991-0614",
     fotoSrc: FotosEquipe.fotoLuquinha,
     opiniao:
       " Tenho me atualizado com as últimas tecnologias e inovações da minha área, e em aprimorar continuamente nossos métodos e processos de fabricação de helicoides para garantir que atendam às necessidades dos nossos clientes.",
@@ -15,7 +15,7 @@ const opinioes = [
   {
     id: 2,
     nome: "João Marcos",
-    whastapp: "(35) 9 9671-9409",
+    whastapp: "(35) 99671-9409",
     fotoSrc: FotosEquipe.fotoJoaoMarcos,
     opiniao:
       "Me sinto motivado pelo desafio de projetar helicoides que sejam resistentes, duráveis e capazes de lidar com as demandas e condições extremas do trabalho agrícola. Estou orgulhoso de poder contribuir para a produção de café e outros alimentos e para o desenvolvimento da economia.",
@@ -23,6 +23,15 @@ const opinioes = [
 ];
 
 export default function Opinioes() {
+  function handleWhastapp(numero: string) {
+    return numero
+      .replace(" ", "")
+      .replace("(", "")
+      .replace(")", "")
+      .replace("-", "")
+      .trim();
+  }
+
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
@@ -44,12 +53,21 @@ export default function Opinioes() {
                 </h2>
                 <p className="text-gray-500">co-fundador / CEO</p>
 
-                <div className="mt-3 flex justify-center items-center space-x-2 bg-green-700 hover:bg-green-800 text-white max-w-56 px-4 py-3 rounded-lg">
-                  <FaWhatsapp className="text-3xl" />
-                  <span className="font-semibold font-leagueSpartan text-xl">
-                    {o.whastapp}
-                  </span>
-                </div>
+                <Link
+                  target="_blank"
+                  href={`https://wa.me/55${handleWhastapp(
+                    o.whastapp
+                  )}?text=Ola, ${
+                    o.nome
+                  }! Vim do site rexhelicoides.com.br e gostaria de mais informações. `}
+                >
+                  <div className="flex justify-center items-center mt-3 space-x-2 bg-green-700 hover:bg-green-800 text-white max-w-56 px-4 py-3 rounded-lg">
+                    <FaWhatsapp className="text-3xl" />
+                    <span className="font-semibold font-leagueSpartan text-xl">
+                      {o.whastapp}
+                    </span>
+                  </div>
+                </Link>
               </div>
             </div>
           ))}
