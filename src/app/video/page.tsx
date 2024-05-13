9cde5cfb9d9379d135ca3a0682e3a9ff37b37f89
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-05-13 13:41:51 -0300

    :lipstick: items center

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,7 +1,7 @@
 export default function VideoPage() {
   function buildVideoCard(url: string, descricao: string) {
     return (
-      <div>
+      <div className="flex flex-col items-center">
         <video
           muted={true}
           className="rounded-xl"
@@ -18,7 +18,7 @@ export default function VideoPage() {
   }
 
   return (
-    <div className="flex flex-col sm:flex-row gap-3 justify-center mt-8 px-2">
+    <div className="mt-8 px-4 sm:px-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 content-center">
       {buildVideoCard(
         "https://res.cloudinary.com/rexhelicoides/video/upload/v1715300620/videos/encaixe-perfeito2_raygd5.mp4",
         "Encaixe Perfeito"
@@ -31,10 +31,17 @@ export default function VideoPage() {
         "https://res.cloudinary.com/rexhelicoides/video/upload/v1715299601/videos/helicoidao_jnhvyb.mp4",
         "Precisão e potência"
       )}
-
       {buildVideoCard(
         "https://res.cloudinary.com/rexhelicoides/video/upload/v1715300622/videos/carreta1_do8lbx.mp4",
         "Acoplada na carreta"
+      )}
+      {buildVideoCard(
+        "https://res.cloudinary.com/rexhelicoides/video/upload/v1715349608/videos/carreta2_uvc7wk.mp4",
+        "Acoplada na carreta"
+      )}
+      {buildVideoCard(
+        "https://res.cloudinary.com/rexhelicoides/video/upload/v1715350104/videos/helicoide1_j8y1am.mp4",
+        "Excelente para seu trabalho"
       )}
     </div>
   );
