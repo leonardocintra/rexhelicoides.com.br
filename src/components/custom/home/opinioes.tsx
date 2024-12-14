ba330ebdb4896ede801dc7dc0ada2ea1109e5571
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-12-14 14:02:36 -0300

    :lipstick: melhoria nos donos

--- src/components/custom/home/opinioes.tsx
+++ src/components/custom/home/opinioes.tsx
@@ -9,16 +9,14 @@ const opinioes = [
     nome: "Lucas Carrijo",
     whastapp: "(35) 99991-0614",
     fotoSrc: FotosEquipe.fotoLuquinha,
-    opiniao:
-      " Tenho me atualizado com as últimas tecnologias e inovações da minha área, e em aprimorar continuamente nossos métodos e processos de fabricação de helicoides para garantir que atendam às necessidades dos nossos clientes.",
+    opiniao: "Representante da Rex Helicoides",
   },
   {
     id: 2,
     nome: "João Marcos",
     whastapp: "(35) 99671-9409",
     fotoSrc: FotosEquipe.fotoJoaoMarcos,
-    opiniao:
-      "Me sinto motivado pelo desafio de projetar helicoides que sejam resistentes, duráveis e capazes de lidar com as demandas e condições extremas do trabalho agrícola. Estou orgulhoso de poder contribuir para a produção de café e outros alimentos e para o desenvolvimento da economia.",
+    opiniao: "Representante da Rex Helicoides",
   },
 ];
 
@@ -34,16 +32,16 @@ export default function Opinioes() {
 
   return (
     <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap -m-4">
+      <div className="px-5 py-24 mx-auto">
+        <div className="flex flex-wrap justify-center gap-3">
           {opinioes.map((o) => (
-            <div key={o.id} className="lg:w-1/2 lg:mb-0 mb-6 p-4">
-              <div className="h-full text-center flex flex-col items-center">
+            <div key={o.id} className="p-10 border rounded-lg border-luqinha shadow-lg">
+              <div className="text-center flex flex-col items-center">
                 <Image
-                  width={500}
-                  height={500}
+                  width={600}
+                  height={600}
                   alt="testimonial"
-                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                  className="h-48 w-48 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
                   src={o.fotoSrc}
                 />
                 <p className="leading-relaxed">{o.opiniao}</p>
